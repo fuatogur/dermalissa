09a0760b674f2f65accea57e008298165e50708a
yusufxx171 <yomapazar@gmail.com>
2026-02-02 12:44:21 +0300

    rgwgwef

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
 
+const favicon = document.getElementById("favicon");
+const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+function updateFavicon(e) {
+  favicon.href = e.matches ? "/favicon-light.svg" : "/favicon-dark.svg";
+}
+
+updateFavicon(darkQuery);
+darkQuery.addEventListener("change", updateFavicon);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
